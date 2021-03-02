d72b9bbc2d54d5dd040acf76307e5747b396e53c
Karvacky-Roma <karvatskij.roma@gmail.com>
2021-03-02 22:47:52 +0200

    made footer section

--- #src/js/main.js
+++ #src/js/main.js
@@ -1,3 +1,35 @@
 // if you need slick slider uncomment this cat
 
-// @@include('slick.min.js') 
+
+$(document).ready(function () {
+	$(".slider-apply").slick({
+		slideToSHow: 1,
+		adaptiveHeight: true,
+		dots: true,
+		arrows: false,
+		infinite: false,
+		easing: 'linear',
+		lazyLoad: 'ondemand',
+		draggable: false,
+		// fade: true,
+	});
+});
+
+let headerNav = document.querySelector('.header__nav');
+let headerBurger = document.querySelector('.header__burger');
+let bodyLock = document.querySelector('body');
+let headerItem = document.querySelector('.header__link');
+
+headerBurger.addEventListener('click', function(e) {
+	e.preventDefault();
+
+	headerNav.classList.toggle('header-active');
+	headerBurger.classList.toggle('header-active');
+	bodyLock.classList.toggle('lock');
+});
+
+//======================================================================================================================================
+// SLick  ==============================================================================================================================
+//======================================================================================================================================
+
+@@include('slick.min.js')
